553f3edaf3b4fe5df7f62b69568de852296041a0
Sanjeevni Srivastava <aec.cse.sanjeevnisrivastava@gmail.com>
2024-03-13 20:26:14 +0530

    Add files via upload

---
+++ func_overload.java
@@ -0,0 +1,19 @@
+public class func_overload {
+    void disp(){
+        System.out.println("No Arguments");
+    }
+    void disp(int x){
+        System.out.println(x);
+    }
+    void disp(int x, int y){
+        System.out.println(x+" "+y);
+    }
+}
+class m_overload{
+    public static void main(String args[]){
+        func_overload ob=new func_overload();
+        ob.disp(50,60);
+        ob.disp();
+        ob.disp(90);
+    }
+}
