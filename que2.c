c145909c9102f4d4654a74c204c2a5ff37d7671f
Sanjeevni Srivastava <aec.cse.sanjeevnisrivastava@gmail.com>
2024-03-20 20:07:55 +0530

    Add files via upload

---
+++ que2.c
@@ -0,0 +1,12 @@
+//Write a program to print three times of a given number
+#include<stdio.h>
+int main(){
+    int num, mul;
+    printf("enter the number:");
+    scanf("%d",&num);
+    mul= 3*num;
+    printf("three times of the number is%d",mul);
+    return 0;
+
+
+}
