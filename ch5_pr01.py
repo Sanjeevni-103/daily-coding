2a25a89b632cfa93aa7798de0f2846d7736323f9
Sanjeevni Srivastava <aec.cse.sanjeevnisrivastava@gmail.com>
2024-04-10 21:07:09 +0530

    Add files via upload

---
+++ ch5_pr01.py
@@ -0,0 +1,10 @@
+Mydict={
+    "pankha":"Fan",
+    "parda":"curtain",
+    "chuha":"mouse"
+}
+print("These are the following options :- ")
+
+print(Mydict.keys())
+a=input("Enter a word\n")
+print(Mydict.get(a))
