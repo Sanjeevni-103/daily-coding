c5cb2def4aa411b015e000fd79c7d4e540de4782
Sanjeevni Srivastava <aec.cse.sanjeevnisrivastava@gmail.com>
2024-04-21 19:22:56 +0530

    Add files via upload

---
+++ lower.java
@@ -0,0 +1,9 @@
+class test
+{
+public static void main(String args[])
+{
+String Str = new String("Subham Shaw");
+System.out.println("Return value : ");
+System.out.println(Str.toLowerCase());
+}
+}
