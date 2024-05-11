255704094386fafe7b78a5c6f92dacd49c07a750
Sanjeevni Srivastava <aec.cse.sanjeevnisrivastava@gmail.com>
2024-05-11 22:22:59 +0530

    Add files via upload

--- array.c
+++ array.c
@@ -1,86 +1,16 @@
+/*write a program to find the sum of all the elements of the array.*/
 #include<stdio.h>
-void traversal (){
-    int a[50],size,i;
-    printf("Enter size of the array :- ");
-    scanf("%d",&size);
-    if(size>50){
-        printf("Enter size within bound limit");
-    }
-    else{
-        printf("Enter elements of the array :-");
-        for (i=0;i<size;i++){
-            scanf("%d",&a[i]);
-        }
-        printf("Elements in array are :- ");
-        for(i=0;i<size;i++){
-            printf("%d ",a[i]);
-        }
-    }    
-}
-void insertion(){
-    int a[50],size,i,pos,num;
-    printf("Enter size of the array :- ");
-    scanf("%d",&size);
-    if(size>50){
-        printf("Enter size within bound limit");
-    }
-    else{
-        printf("Enter elements of the array :-");
-        for (i=0;i<size;i++){
-            scanf("%d",&a[i]);
-        }
-        printf("Enter data you want to insert :- ");
-        scanf("%d",&num);
-        printf("Enetr the position where the data is to be inserted :- ");
-        scanf("%d",&pos);
-        if(pos<=0 || pos>size+1){
-            printf("Invalid position !!! ");
-        }
-        else{
-            for(i=size-1;i>=pos-1;i--){
-                a[i+1]=a[i];
-            }
-            a[pos-1]=num;
-            size++;
-            printf("Elements in array are :- ");
-            for(i=0;i<size;i++){
-                printf("%d ",a[i]);
-            }
-        }
-    }  
-}
-void deletion(){
-    int a[50],size,i,pos;
-    printf("Enter size of the array :- ");
-    scanf("%d",&size);
-    if(size>50){
-        printf("Enter size within bound limit");
-    }
-    else{
-        printf("Enter elements of the array :-");
-        for (i=0;i<size;i++){
-            scanf("%d",&a[i]);
-        }
-        printf("Enter the position where element is to be deleted :- ");
-        scanf("%d",&pos);
-        if(pos<=0 || pos>size ){
-            printf("Invalid position!!!");
-        }
-        else{
-            for(i=pos-1;i<size-1;i++){
-                a[i]=a[i+1];
-            }
-            size--;
-            printf("Elemnts of the array are :- ");
-            for (i=0;i<size;i++){
-                printf("%d ",a[i]);
-            }
-        }
-    }    
-}
 int main(){
-    //traversal();
-    //insertion();
-    deletion();
-    return 0;
+    int arr[5];
+    int sum=0;
+    printf("Enter the elements of the array :- ");
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    for(int i=0;i<5;i++){
+        sum=sum+arr[i];
+    }
+    printf("Sum of the elements of the array is %d ",sum);
 }
+
